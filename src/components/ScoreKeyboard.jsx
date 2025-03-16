40698a9f2950174f97e2ee1ada2202073cdf95ac
CattTAT <chinchin2611@gmail.com>
2025-03-17 01:53:41 +0800

    add score keyboard

--- src/components/ScoreKeyboard.jsx
+++ src/components/ScoreKeyboard.jsx
@@ -1,28 +1,131 @@
-import React from 'react';
-import styled from '@emotion/styled';
-import { Grid2 } from '@mui/material';
+import React, { forwardRef } from "react";
+import styled from "@emotion/styled";
+import { Button, Grid2, Stack, IconButton } from "@mui/material";
+import { getScoreColor } from "./ScoreTable";
+import {
+  ArrowLeft,
+  ArrowRight,
+  Backspace,
+  KeyboardHide,
+} from "@mui/icons-material";
+import { Icon } from "@iconify/react";
 
+const ScoreKeyboard = forwardRef(({ visibility, targetFace }, ref) => {
+  const Keyboard = styled(Grid2)(() => ({
+    backgroundColor: "#D8D8D8",
+    position: "fixed",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    width: "100%",
+    display: "flex",
+    visibility: visibility ? "visible" : "hidden",
+    zIndex: 10,
+    height: "30%",
+    boxShadow: "0px -2px 5px 0px #D8D8D8",
+    padding: "8px",
+  }));
 
-function ScoreKeyboard ({visibility}){
+  const ScoreButton = styled(Button)(({ theme }) => ({
+    fontSize: "30px",
+    borderRadius: 10,
+    border: "1px solid #D8D8D8",
+    maxHeight: "25%",
+    maxWidth: "25%",
+  }));
 
-    const Keyboard = styled(Grid2)(() => ({
-        backgroundColor: "#D8D8D8",
-        position: "fixed",
-        bottom: 0,
-        left: 0,
-        right: 0,
-        width: "100%",
-        display: "grid",
-        visibility: visibility ? "visible" : "hidden",
-        zIndex: 10,
-        height: "30%",
-    }));
-    
+  const ControlButton = styled(Button)(({ theme }) => ({
+    fontSize: "20px",
+    borderRadius: 10,
+    border: "1px solid #D8D8D8",
+    color: "white",
+    flexDirection: "column",
+    backgroundColor: "#505050",
+    width: "100%",
+    lineHeight: "1.5",
+    margin: 0,
+    "& .MuiButton-startIcon": {
+      margin: 0,
+    },
+    ".css-1sh91j5-MuiButton-startIcon>*:nth-of-type(1)": {
+      fontSize: "30px",
+    },
+  }));
 
-    return (
-        <Keyboard className="score-keyboard">
-        </Keyboard>
-    )
+  return (
+    <Keyboard direction="row" className="score-keyboard" ref={ref}>
+      <Stack
+        direction="row"
+        flexWrap="wrap"
+        alignContent="space-around"
+        gap={0.5}
+        width="70%"
+      >
+        {getTargetFaceScore(targetFace).map((score) => (
+          <ScoreButton
+            key={score}
+            sx={{
+              backgroundColor: getScoreColor(score),
+              color: getScoreColor(score) === "black" ? "white" : "black",
+            }}
+          >
+            {score}
+          </ScoreButton>
+        ))}
+      </Stack>
+      <Stack
+        direction="row"
+        flexWrap="wrap"
+        justifyContent="space-around"
+        gap={1}
+        width="30%"
+        px={0.5}
+      >
+        <ControlButton
+          startIcon={<Icon icon="material-symbols:backspace-outline" />}
+        >
+          Backspace
+        </ControlButton>
+        <IconButton
+          sx={{
+            width: "40% !important",
+            backgroundColor: "#505050 !important",
+            borderRadius: "10px",
+            color: "white",
+          }}
+        >
+          <Icon icon="ooui:previous-ltr" />
+        </IconButton>
+        <IconButton
+          sx={{
+            width: "40% !important",
+            backgroundColor: "#505050 !important",
+            borderRadius: "10px",
+            color: "white",
+          }}
+        >
+          <Icon icon="ooui:next-ltr" />
+        </IconButton>
+        <ControlButton
+          startIcon={<Icon icon="material-symbols:keyboard-hide-outline" />}
+        >
+          Hide Keyboard
+        </ControlButton>
+      </Stack>
+    </Keyboard>
+  );
+});
+
+function getTargetFaceScore(targetFace) {
+  if (targetFace.includes("10 Rings")) {
+    return ["X", 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, "M"];
+  } else if (targetFace.includes("6 Rings")) {
+    return ["X", 10, 9, 8, 7, 6, 5, "M"];
+  } else if (targetFace.includes("5 Rings")) {
+    return ["X", 10, 9, 8, 7, 6, "M"];
+  }
 }
+
+ScoreKeyboard.displayName = "ScoreKeyboard";
 
 export default ScoreKeyboard;
--- src/components/ScoreTable.jsx
+++ src/components/ScoreTable.jsx
@@ -1,5 +1,5 @@
 import { GlobalStyles, Stack } from "@mui/material";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import ScoreKeyboard from "./ScoreKeyboard";
 
 const ScoreTable = ({ round, sets, arrows, distance, targetFace }) => {
@@ -13,6 +13,8 @@ const ScoreTable = ({ round, sets, arrows, distance, targetFace }) => {
 
   const [scores, setScores] = useState(testScores);
   const [selectedCell, setSelectedCell] = useState(null);
+  const tableRef = useRef(null);
+  const keyboardRef = useRef(null);
 
   const scoreTotal = scores
     .map((r) => r.reduce(addScore, 0))
@@ -24,117 +26,153 @@ const ScoreTable = ({ round, sets, arrows, distance, targetFace }) => {
       .reduce((a, b) => a + b, 0);
   };
 
+  const handleCellClick = (row, col) => {
+    setSelectedCell({ row, col });
+  };
+
+  const handleClickOutside = (event) => {
+    if (event.target.dataset.display) {
+      setSelectedCell(null);
+    }
+    if (
+      tableRef.current &&
+      !tableRef.current.contains(event.target) &&
+      !(keyboardRef.current && keyboardRef.current.contains(event.target))
+    ) {
+      setSelectedCell(null);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <>
-      <Stack>
-        <table>
-          <GlobalStyles
-            styles={{
-              "table, th, td": {
-                borderCollapse: "collapse",
-                border: "1px solid black",
-                fontSize: 24,
-              },
-              td: {
-                textAlign: "center",
-                minHeight: 35,
-              },
-            }}
-          />
-          <thead>
-            <tr>
-              <td colSpan={4}>
-                {distance} Round {round}
-              </td>
-              <td style={{ fontSize: 12 }}>3 arrows subtotal</td>
-              <td style={{ fontSize: 12 }}>6 arrow subtotal</td>
-            </tr>
-          </thead>
-          <tbody>
-            {Array(totalRows)
-              .fill()
-              .map((_, r) => {
-                const isEven = (r + 1) % 2 === 0;
-                const currentSubset = (r + 1) * 3;
-                const headerText =
-                  currentSubset < 10 ? `0${currentSubset}` : currentSubset;
+    <Stack>
+      <table ref={tableRef}>
+        <GlobalStyles
+          styles={{
+            "table, th, td": {
+              borderCollapse: "collapse",
+              border: "1px solid black",
+              fontSize: 24,
+            },
+            td: {
+              textAlign: "center",
+              minHeight: 35,
+            },
+          }}
+        />
+        <thead>
+          <tr>
+            <td data-display={true} colSpan={4}>
+              {distance} Round {round}
+            </td>
+            <td data-display={true} style={{ fontSize: 12 }}>
+              3 arrows subtotal
+            </td>
+            <td data-display={true} style={{ fontSize: 12 }}>
+              6 arrow subtotal
+            </td>
+          </tr>
+        </thead>
+        <tbody>
+          {Array(totalRows)
+            .fill()
+            .map((_, r) => {
+              const isEven = (r + 1) % 2 === 0;
+              const currentSubset = (r + 1) * 3;
+              const headerText =
+                currentSubset < 10 ? `0${currentSubset}` : currentSubset;
 
-                return (
-                  <tr key={r}>
-                    {/* row header */}
-                    <td
-                      style={{
-                        background: "#E0E0E0",
-                        width: 45,
-                      }}
-                    >
-                      {headerText}
-                    </td>
+              return (
+                <tr key={r}>
+                  {/* row header */}
+                  <td
+                    data-display={true}
+                    style={{
+                      background: "#E0E0E0",
+                      width: 45,
+                    }}
+                  >
+                    {headerText}
+                  </td>
 
-                    {/* scores */}
-                    {Array(3)
-                      .fill()
-                      .map((_, a) => {
-                        const s = scores?.[r]?.[a] ?? "-";
-                        const bgcolor = getScoreColor(s);
-                        const isCurrentCell =
-                          selectedCell?.r === r && selectedCell?.a === a;
+                  {/* scores */}
+                  {Array(3)
+                    .fill()
+                    .map((_, a) => {
+                      const s = scores?.[r]?.[a] ?? "-";
+                      const bgcolor = getScoreColor(s);
+                      const isCurrentCell =
+                        selectedCell?.row === r && selectedCell?.col === a;
 
-                        return (
-                          <td
-                            key={a}
-                            style={{
-                              width: 45,
-                              background: bgcolor,
-                              color: bgcolor === "black" ? "white" : "black",
-                              border: isCurrentCell
-                                ? "2px solid red"
-                                : "1px solid black",
-                            }}
-                            onClick={() => {
-                              setSelectedCell({ r, a });
-                            }}
-                          >
-                            {s}
-                          </td>
-                        );
-                      })}
-
-                    {/* subset subtotal */}
-                    <td>{scores?.[r]?.reduce(addScore, 0) ?? 0}</td>
-
-                    {
-                      // If current row is even, display the total of the 2 subsets (1 set)
-                      isEven ? (
-                        <td>
-                          {(scores?.[r]?.reduce(addScore, 0) ?? 0) +
-                            (scores?.[r - 1]?.reduce(addScore, 0) ?? 0)}
+                      return (
+                        <td
+                          key={a}
+                          data-row={r}
+                          data-col={a}
+                          data-display={false}
+                          style={{
+                            width: 45,
+                            background: bgcolor,
+                            color: bgcolor === "black" ? "white" : "black",
+                            border: isCurrentCell
+                              ? "2px solid red"
+                              : "1px solid black",
+                          }}
+                          onClick={() => handleCellClick(r, a)}
+                        >
+                          {s}
                         </td>
-                      ) : (
-                        // if odd , display black cell
-                        <td style={{ background: "black" }}>&nbsp;</td>
-                      )
-                    }
-                  </tr>
-                );
-              })}
+                      );
+                    })}
 
-            <tr>
-              <td colSpan={5} style={{ textAlign: "left" }}>
-                <Stack direction="row" gap={1} width="100%" px={0.5}>
-                  <span>X&apos;s: {count("X")}</span>
-                  <span>10&apos;s: {count(10)}</span>
-                  <span style={{ marginLeft: "auto" }}>Total</span>
-                </Stack>
-              </td>
-              <td>{scoreTotal}</td>
-            </tr>
-          </tbody>
-        </table>
-        <span style={{ fontSize: 12 }}>Last Modified: </span>
-      </Stack>
-      <ScoreKeyboard visibility={selectedCell !== null} />
-    </>
+                  {/* subset subtotal */}
+                  <td data-display={true}>
+                    {scores?.[r]?.reduce(addScore, 0) ?? 0}
+                  </td>
+
+                  {
+                    // If current row is even, data-display the total of the 2 subsets (1 set)
+                    isEven ? (
+                      <td data-display={true}>
+                        {(scores?.[r]?.reduce(addScore, 0) ?? 0) +
+                          (scores?.[r - 1]?.reduce(addScore, 0) ?? 0)}
+                      </td>
+                    ) : (
+                      // if odd , data-display black cell
+                      <td data-display={true} style={{ background: "black" }}>
+                        &nbsp;
+                      </td>
+                    )
+                  }
+                </tr>
+              );
+            })}
+
+          <tr>
+            <td data-display={true} colSpan={5} style={{ textAlign: "left" }}>
+              <Stack direction="row" gap={1} width="100%" px={0.5}>
+                <span>X&apos;s: {count("X")}</span>
+                <span>10&apos;s: {count(10)}</span>
+                <span style={{ marginLeft: "auto" }}>Total</span>
+              </Stack>
+            </td>
+            <td data-display={true}>{scoreTotal}</td>
+          </tr>
+        </tbody>
+      </table>
+      <span style={{ fontSize: 12 }}>Last Modified: </span>
+      <ScoreKeyboard
+        visibility={selectedCell !== null}
+        targetFace={targetFace}
+        ref={keyboardRef}
+      />
+    </Stack>
   );
 };
 
@@ -143,18 +181,18 @@ function getScoreColor(score) {
     case "X":
     case 10:
     case 9:
-      return "yellow";
+      return "#FFF946";
     case 8:
     case 7:
-      return "red";
+      return "#FF5959";
     case 6:
     case 5:
-      return "blue";
+      return "#4D99E5";
     case 4:
     case 3:
       return "black";
     case "M":
-      return "grey";
+      return "#ABABAB";
     default:
       return "white";
   }
@@ -176,3 +214,4 @@ function getRealScore(score) {
 }
 
 export default ScoreTable;
+export { getScoreColor };
