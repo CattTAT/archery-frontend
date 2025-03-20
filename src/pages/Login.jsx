5dab138a26b2e6dd5de0027fb8205d7e119778e0
CattTAT <chinchin2611@gmail.com>
2025-03-20 16:58:32 +0800

    implement first registration logic with userId in store

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,21 +2,25 @@ import React, { useEffect, useState } from 'react';
 import Home from './Home';
 import Personal from './Personal';
 import { v4 } from 'uuid';
+import { useSnapshot } from "valtio";
+import { store } from "../lib/store";
+
 
 const Login = () => {
     // add userID to check if user is new
   const [newUser, setNewUser] = useState(false);
+  const userId = useSnapshot(store).userId;
 
   useEffect(() => {
     const deviceId = localStorage.getItem("deviceId");
-    if (!deviceId) {
+    if (!deviceId || userId === "") {
       localStorage.setItem("deviceId", v4());
       setNewUser(true);
     }
   }, []);
 
   return (
-    newUser ? <Personal  isRegistration={true} /> : <Home />
+    newUser ? <Personal isRegistration={true} /> : <Home />
   );
 }
 
