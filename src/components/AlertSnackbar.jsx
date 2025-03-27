7043ad5893d96d1ef1db6cdeb104e76a97d27978
CattTAT <chinchin2611@gmail.com>
2025-03-27 11:36:02 +0800

    change the logic of opening the alertsnackbar to control with localstorage

--- src/components/AlertSnackbar.jsx
+++ src/components/AlertSnackbar.jsx
@@ -1,15 +1,32 @@
+import React, { useEffect } from "react";
 import { Snackbar, Alert } from "@mui/material";
 
-export default function AlertSnackbar({ open, handleClose, message }) {
+export default function AlertSnackbar() {
+  const [alertMessage, setAlertMessage] = React.useState("");
+  const [openAlert, setOpenAlert] = React.useState(false);
+  useEffect(() => {
+    const message = localStorage.getItem("alertMessage");
+    if (message) {
+      setAlertMessage(message);
+      setOpenAlert(true);
+      localStorage.removeItem("alertMessage"); // Clear the alert message after displaying it
+    }
+  }, []);
   return (
-    <Snackbar open={open} autoHideDuration={3000} onClose={handleClose} anchorOrigin={{ vertical: "bottom", horizontal: "right" }} sx={{marginBottom: "100px"}}>
+    <Snackbar
+      open={openAlert}
+      autoHideDuration={3000}
+      onClose={() => setOpenAlert(false)}
+      anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+      sx={{ marginBottom: "100px" }}
+    >
       <Alert
-        onClose={handleClose}
+        onClose={() => setOpenAlert(false)}
         severity="success"
         variant="filled"
         sx={{ width: "100%" }}
       >
-        {message}
+        {alertMessage}
       </Alert>
     </Snackbar>
   );
